63cf1d4481b40cbc2e16d6655a95a8cfefefe069
zahur76 <zahurmeerun@hotmail.com>
2020-12-30 11:28:14 +0000

    Create basic view for all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+def all_products(request):
+    """ A view to return all products and product search """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
