ed9cc040a276a11ff12107e2af2d70b5686a137b
zahur76 <zahurmeerun@hotmail.com>
2020-12-30 16:33:25 +0000

    Add get request for main dish category

--- products/views.py
+++ products/views.py
@@ -6,8 +6,15 @@ from .models import Product, Category
 def all_products(request):
     """ A view to return all products and product search """
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            products = products.filter(category__name=categories)            
 
     context = {
         'products': products,
+        'current_category': categories,
     }
     return render(request, 'products/products.html', context)
