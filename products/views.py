f1f068969fbf68e466fd7bfc52d0df1c41eb9a4d
zahur76 <zahurmeerun@hotmail.com>
2020-12-30 13:11:22 +0000

    Make migrations for product app

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product, Category
 
+
 # Create your views here.
 def all_products(request):
     """ A view to return all products and product search """
