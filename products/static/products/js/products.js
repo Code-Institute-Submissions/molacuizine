eec77e4540cdd0405fcb8843ab4a1d1d8e2f066d
zahur76 <zahurmeerun@hotmail.com>
2020-12-31 10:29:29 +0000

    Add code to select quantity using sign buttons

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-  $("#minus").click(function(){
-    $("body").css('background', 'red');
-  });
-  $("#plus").click(function(){
-    $("body").css('background', 'blue');
-  });
+$(document).ready(function(){    
+    let val=0;           
+    $("#plus").click(function(){
+        if(val<=9 && val >=0){
+            val += 1;
+            $("#quantity").val(val);
+        }                       
+    });    
+    $("#minus").click(function(){ 
+        if(val<=10 && val >=1){            
+            val -= 1;
+            $("#quantity").val(val);
+        }      
+    });         
 });
