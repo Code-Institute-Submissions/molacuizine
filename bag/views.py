1b99a10f3e3b44f00f15fcc3566d2dfecf19ed9f
zahur76 <zahurmeerun@hotmail.com>
2020-12-31 12:49:34 +0000

    Create view and urls for add_to_bag function

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
 
 # Create your views here.
-def 
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, product_id):
+    """ A view that renders the bag contents page """
+
+    return redirect(reverse('product_detail', args=[product_id]))
