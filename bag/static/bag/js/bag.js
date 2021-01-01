3d11167f264b02cb3fc3583944bd4d1f7150178f
zahur76 <zahurmeerun@hotmail.com>
2021-01-01 14:00:42 +0000

    Fix issue where quantity selector did not work in medium devices and smaller

--- bag/static/bag/js/bag.js
+++ bag/static/bag/js/bag.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Code to allow selection on bag page //
-
+    // medium and large screens //
     $(".plus").click(function(){ 
         let plus=$(this).attr("id");
         let newPlus=plus.split("-")
@@ -19,5 +19,26 @@ $(document).ready(function(){
             val -= 1;
             $("#quantity-"+newMinus[1]).val(val);
         }      
-    });         
+    }); 
+    
+    // Medim and lower screens //
+    $(".plus-small").click(function(){ 
+        let plus=$(this).attr("id");
+        let newPlus=plus.split("-")
+        let val=parseInt($("#quantity-small-"+newPlus[2]).val()); 
+        console.log(val)
+        if(val<=49){
+            val += 1;
+            $("#quantity-small-"+newPlus[2]).val(val);
+        }                       
+    });    
+    $(".minus-small").click(function(){
+        let minus=$(this).attr("id");
+        let newMinus=minus.split("-")
+        let val=parseInt($("#quantity-small-"+newMinus[2]).val()); 
+        if(val >=2){            
+            val -= 1;
+            $("#quantity-small-"+newMinus[2]).val(val);
+        }      
+    })
 });
