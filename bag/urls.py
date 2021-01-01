b701eda104294dd4f094e06585fa1d3f4831083b
zahur76 <zahurmeerun@hotmail.com>
2021-01-01 13:04:16 +0000

    Add url to delete item

--- bag/urls.py
+++ bag/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from bag import views
-# from . import views
+
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<product_id>/', views.add_to_bag, name='add_to_bag'),
     path('edit/<product_id>/', views.adjust_bag, name='adjust_bag'),
+    path('delete/<product_id>/', views.delete_from_bag, name='delete_item'),
 ]
