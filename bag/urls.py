a225d03275c3d76bde34f0a7810755008a35b39c
zahur76 <zahurmeerun@hotmail.com>
2021-01-01 12:41:15 +0000

    Create adjust bag url

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,5 @@ from bag import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<product_id>/', views.add_to_bag, name='add_to_bag'),
+    path('edit/<product_id>/', views.add_to_bag, name='adjust_bag'),
 ]
