3a946f6418f51b209405caa2dbee840b39a946ea
zahur76 <zahurmeerun@hotmail.com>
2021-01-02 14:38:45 +0000

    Create view to handle category search with pagination

--- products/views.py
+++ products/views.py
@@ -15,7 +15,6 @@ def all_products(request):
             categories = request.GET['category']
             products = products.filter(category__name=categories)
             categories = Category.objects.filter(name=categories)
-            print(categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -26,17 +25,26 @@ def all_products(request):
                 queries = Q(
                     name__icontains=query) | Q(description__icontains=query)
                 products = products.filter(queries)
+
     count = products.count()
 
-    ''' Add pagination to product view page'''
+    # Add pagination numbers and links to product page
+
+    if 'page_number' in request.GET:
+        page_number = int(request.GET['page_number'])
+
+    else:
+        page_number = 1
 
     objects = products
+
     p = Paginator(objects, 5)
-    print(p.count)
-    print(p.num_pages)
+    page_num = list(range(1, p.num_pages+1))
+    objects = objects[(page_number-1)*5:((page_number-1)*5)+5]
 
     context = {
-        'products': products,
+        'page_num': page_num,
+        'objects': objects,
         'current_category': categories,
         'count': count,
     }
@@ -50,3 +58,36 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+
+def category_search(request):
+    """ A view to return products when searching with pagination"""
+
+    products = Product.objects.all()
+    category = None
+
+    if 'category' in request.GET:
+        category = request.GET['category']
+        products = products.filter(category__name=category)
+        friendly_name = (get_object_or_404(
+            Category, name=category)).friendly_name
+    if 'page_number' in request.GET:
+        page_number = int(request.GET['page_number'])
+    else:
+        page_number = 1
+    objects = products
+
+    p = Paginator(objects, 5)
+    page_num = list(range(1, p.num_pages+1))
+    objects = objects[(page_number-1)*5:((page_number-1)*5)+5]
+
+    count = products.count()
+
+    context = {
+        'page_num': page_num,
+        'objects': objects,
+        'current_category': category,
+        'friendly_name': friendly_name,
+        'count': count,
+    }
+    return render(request, 'products/category_search.html', context)
