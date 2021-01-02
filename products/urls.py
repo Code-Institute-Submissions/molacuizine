41965bae3f4b5aaaf87271526583a4c88c921523
zahur76 <zahurmeerun@hotmail.com>
2021-01-02 14:38:11 +0000

    Create view for search page pagination

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from products import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>', views.product_detail, name='product_detail'),
+    path('category_search', views.category_search, name='category_search'),
 ]
