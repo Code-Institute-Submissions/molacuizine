1a2067bc6673007c122e7a015d1d185ad52cb949
zahur76 <zahurmeerun@hotmail.com>
2021-01-02 15:20:40 +0000

    Add view to handle search bar queries with pagination

--- products/views.py
+++ products/views.py
@@ -6,25 +6,9 @@ from django.db.models import Q
 
 # Create your views here.
 def all_products(request):
-    """ A view to return all products and product search """
+    """ A view to return all products  """
     products = Product.objects.all()
     categories = None
-
-    if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category']
-            products = products.filter(category__name=categories)
-            categories = Category.objects.filter(name=categories)
-
-        if 'q' in request.GET:
-            query = request.GET['q']
-            # Query is blank query= ""
-            if not query:
-                return redirect(reverse('products'))
-            else:
-                queries = Q(
-                    name__icontains=query) | Q(description__icontains=query)
-                products = products.filter(queries)
 
     count = products.count()
 
@@ -51,17 +35,45 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request, product_id):
-    """ A view to return product a with specific id/pk """
-    product = get_object_or_404(Product, pk=product_id)
+def query_search(request):
+    """ A view to return products when searching using search bar
+     with pagination"""
+
+    products = Product.objects.all()
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        # Query is blank query= ""
+        if not query:
+            return redirect(reverse('products'))
+        else:
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    if 'page_number' in request.GET:
+        page_number = int(request.GET['page_number'])
+    else:
+        page_number = 1
+    objects = products
+
+    p = Paginator(objects, 5)
+    page_num = list(range(1, p.num_pages+1))
+    objects = objects[(page_number-1)*5:((page_number-1)*5)+5]
+
+    count = products.count()
+
     context = {
-        'product': product,
+        'page_num': page_num,
+        'objects': objects,
+        'count': count,
+        'query': query,
     }
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/query_search.html', context)
 
 
 def category_search(request):
-    """ A view to return products when searching with pagination"""
+    """ A view to return products when searching category with pagination"""
 
     products = Product.objects.all()
     category = None
@@ -91,3 +103,12 @@ def category_search(request):
         'count': count,
     }
     return render(request, 'products/category_search.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to return product a with specific id/pk """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
