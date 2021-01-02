880498e252ba6e209003003ea044452fb695cc38
zahur76 <zahurmeerun@hotmail.com>
2021-01-02 15:15:53 +0000

    Add url for new query search view

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('category_search', views.category_search, name='category_search'),
+    path('query_search', views.query_search, name='query_search'),
 ]
