c26b23c330145b5b0ca2593aa77adea43e6123b2
zahur76 <zahurmeerun@hotmail.com>
2021-01-03 10:28:26 +0000

    Add crispy form settings to main settings file

--- mo_lacuizine/settings.py
+++ mo_lacuizine/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mo_lacuizine.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -78,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
