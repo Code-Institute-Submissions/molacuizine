5ff1465ec52f75c4dff2f9f3d2d42a5498d5d587
zahur76 <zahurmeerun@hotmail.com>
2021-01-03 08:15:06 +0000

    Remove print statement from bag view

--- bag/views.py
+++ bag/views.py
@@ -30,7 +30,7 @@ def add_to_bag(request, product_id):
             messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
-    print(bag)
+    
     return redirect(reverse('product_detail', args=[product_id]))
 
 
