c0ade894038136c4bb48915f8047bf2d6874cc5e
zahur76 <zahurmeerun@hotmail.com>
2021-01-04 11:44:00 +0000

    Add code to handle post requests when profile form updated

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
@@ -11,7 +12,20 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user__username=request.user)
 
-    form = UserProfileForm(instance=profile)
+    if request.method == 'POST':
+
+        form = UserProfileForm(request.POST, instance=profile)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
+
+    if request.method == 'GET':
+
+        form = UserProfileForm(instance=profile)
 
     context = {
         'form': form,
