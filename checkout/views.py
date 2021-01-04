45b4f08813055d66c26f983d9073237aa3aa12d5
zahur76 <zahurmeerun@hotmail.com>
2021-01-04 17:09:58 +0000

    Add logic to prevent users from brute forcing in to empty checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import OrderForm
 from profiles.models import UserProfile
 
@@ -8,6 +9,11 @@ from profiles.models import UserProfile
 @login_required
 def checkout(request):
     """ view to display checkout page """
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
 
     profile = get_object_or_404(UserProfile, user__username=request.user)
 
