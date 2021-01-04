0d660e123a9ae6bbad3ed3f1f789068407143839
zahur76 <zahurmeerun@hotmail.com>
2021-01-04 12:30:21 +0000

    Create basic view for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def checkout(request):
+    """ view to display checkout page """
+
+    context = {
+    }
+    return render(request, 'checkout/checkout.html', context)
