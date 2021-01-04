81f924712ee061e8029c65ae7a72e24d445ba079
zahur76 <zahurmeerun@hotmail.com>
2021-01-04 16:29:17 +0000

    Setup up initial GET view for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .forms import OrderForm
+from profiles.models import UserProfile
 
 
 # Create your views here.
@@ -7,6 +9,18 @@ from django.contrib.auth.decorators import login_required
 def checkout(request):
     """ view to display checkout page """
 
+    profile = get_object_or_404(UserProfile, user__username=request.user)
+
+    order_form = OrderForm(initial={
+        'full_name': profile.user.first_name + " " + profile.user.last_name,
+        'email': profile.user.email,
+        'phone_number': profile.default_phone_number,
+        'postcode': profile.default_postcode,
+        'town': profile.town,
+        'street_address': profile.default_street_address1,
+        })
+
     context = {
+        'order_form': order_form,
     }
     return render(request, 'checkout/checkout.html', context)
