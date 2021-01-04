9da3e8623debebe78a057cd58a045b08ce27b677
zahur76 <zahurmeerun@hotmail.com>
2021-01-04 10:44:43 +0000

    Remove profile js code

--- static/js/base.js
+++ static/js/base.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     // Required to display toasts //    
     $('.toast').toast('show');
     
+    // Required to add paceholders to registration page //
     $('#id_first_name').attr('placeholder', "Firstname"); 
-    $('#id_last_name').attr('placeholder', "Lastname");     
-});
+    $('#id_last_name').attr('placeholder', "Lastname");    
+}); 
