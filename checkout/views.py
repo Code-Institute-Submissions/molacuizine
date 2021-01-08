7e56f14ca3c15cb5b2a8248699c3e1a6e0abca24
zahur76 <zahurmeerun@hotmail.com>
2021-01-08 09:33:28 +0000

    Create post request logic

--- checkout/views.py
+++ checkout/views.py
@@ -3,28 +3,39 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import OrderForm
 from profiles.models import UserProfile
+from bag.contexts import bag_contents
+import json
 
 
 # Create your views here.
 @login_required
 def checkout(request):
     """ view to display checkout page """
-
+    
     bag = request.session.get('bag', {})
+
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     profile = get_object_or_404(UserProfile, user__username=request.user)
 
-    order_form = OrderForm(initial={
-        'full_name': profile.user.first_name + " " + profile.user.last_name,
-        'email': profile.user.email,
-        'phone_number': profile.default_phone_number,
-        'postcode': profile.default_postcode,
-        'town': profile.town,
-        'street_address': profile.default_street_address1,
-        })
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.original_bag = json.dumps(bag)
+            order.save()
+    # Get request
+    else:
+        order_form = OrderForm(initial={
+            'full_name': profile.user.first_name + " " + profile.user.last_name,
+            'email': profile.user.email,
+            'phone_number': profile.default_phone_number,
+            'postcode': profile.default_postcode,
+            'town': profile.town,
+            'street_address': profile.default_street_address1,
+            })
 
     context = {
         'order_form': order_form,
