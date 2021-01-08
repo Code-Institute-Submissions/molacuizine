e6bd33c66a5f540d93271a7c533431a9f9e8bc3b
zahur76 <zahurmeerun@hotmail.com>
2021-01-08 13:23:06 +0000

    Create checkout success url

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,7 @@ from checkout import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path(
+        'checkout_succcess/<order_number>',
+        views.checkout_success, name='checkout_success'),
 ]
