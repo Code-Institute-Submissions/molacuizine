dadc3012c70baf0b14f99af000ee106f11c8d5db
zahur76 <zahurmeerun@hotmail.com>
2021-01-09 09:02:31 +0000

    Provide proper indentation to remove line too long error

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from profiles import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history', views.order_history, name='order_history'),
+    path('order_history/<order_number>', views.order_history,
+         name='order_history'),
 ]
