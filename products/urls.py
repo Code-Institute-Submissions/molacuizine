c59ea0ca14a0a8ad5ef17504de85c6cce20ff863
zahur76 <zahurmeerun@hotmail.com>
2021-01-09 10:00:15 +0000

    Add url for store management in product app

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('category_search', views.category_search, name='category_search'),
     path('query_search', views.query_search, name='query_search'),
+    path('store_management', views.store_management, name='store_management'),
 ]
