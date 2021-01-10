e4206d025a08fe5c47393bf89db326f785577919
zahur76 <zahurmeerun@hotmail.com>
2021-01-10 10:53:23 +0000

    Remove white space

--- bag/models.py
+++ bag/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Store(models.Model):
     ''' Model to retain store status'''
-    
+
     store_status = models.CharField(max_length=5)
 
     def __str__(self):
