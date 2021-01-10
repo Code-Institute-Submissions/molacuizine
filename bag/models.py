fb3394bed1c6f9468ca3961de72633959c223f01
zahur76 <zahurmeerun@hotmail.com>
2021-01-10 08:40:02 +0000

    Create Store status model

--- bag/models.py
+++ bag/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Store(models.Model):
+    store_status = models.BooleanField(default=False, null=False, blank=False)
+
+    def __str__(self):
+        return self.store_status
