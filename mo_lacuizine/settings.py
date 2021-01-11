8dcd993d5844f598f9a5cf6279d5ec56074909bc
zahur76 <zahurmeerun@hotmail.com>
2021-01-11 07:35:14 +0000

    Add cache settings for media and static file

--- mo_lacuizine/settings.py
+++ mo_lacuizine/settings.py
@@ -184,6 +184,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'mo-lacuizine'
     AWS_S3_REGION_NAME = 'ap-southeast-1'
