f85b65a24065fcb41a9d53d9d31a0cc1586cf25c
zahur76 <zahurmeerun@hotmail.com>
2021-01-13 16:02:43 +0000

    Add stripe pid to order once submitted

--- checkout/views.py
+++ checkout/views.py
@@ -58,6 +58,8 @@ def checkout(request):
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
             order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.user_profile = profile
             order.save()
