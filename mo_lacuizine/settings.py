da61e176d80eca6f04c091cf90a96c8ab32e2385
zahur76 <zahurmeerun@hotmail.com>
2021-01-14 08:43:58 +0000

    Add google api key to env file

--- mo_lacuizine/settings.py
+++ mo_lacuizine/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
