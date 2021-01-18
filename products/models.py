a5bf696b94da5647f28ea177a8dc329a9557bd69
zahur76 <zahurmeerun@hotmail.com>
2021-01-18 10:38:51 +0000

    Add spice index to product model

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
             related_name='products')
     name = models.CharField(max_length=254)
     description = models.TextField()
+    spice_index = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
