0f0ab34a3ad463825c6f0552837500664ffcd050
zahur76 <zahurmeerun@hotmail.com>
2021-01-19 09:59:49 +0000

    Add towns to home view to show on home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from profiles.models import Town
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    towns = Town.objects.all()
+    context = {
+        'towns': towns
+    }
+    return render(request, 'home/index.html', context)
