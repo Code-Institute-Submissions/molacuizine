a24181cf923a651362988a755ecbc72ba633275e
zahur76 <zahurmeerun@hotmail.com>
2021-01-19 09:40:14 +0000

    Add function to close delievry info modal

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $(".open").click(function(){
         $("#deliveryModal").modal('show');
     })
+
+    $(".close").click(function(){
+        $("#deliveryModal").modal('hide');
+    })
 });
