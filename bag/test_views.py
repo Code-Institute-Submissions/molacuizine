6d4aff2ae1cbdc1791648f946b6ee2ef03253fa8
zahur76 <zahurmeerun@hotmail.com>
2021-01-21 12:29:51 +0000

    Add test for adjusting bag content when spice index included

--- bag/test_views.py
+++ bag/test_views.py
@@ -33,8 +33,68 @@ class BagTestViews(TestCase):
         self.assertEqual(len(bag), 1)
         self.assertRedirects(response, f'/products/{product.id}')
 
+    def test_add_to_bag_with_different_spice_index_product(self):
+        product = Product.objects.create(
+                name="Item", price=50, category_id=1, spice_index=True)
+        post_data = {
+            'quantity': '1',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data)
+        post_data_two = {
+            'quantity': '1',
+            'spice_index': 'hot',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data_two)
+        bag = self.client.session.get('bag', {})        
+        self.assertEqual(len(bag), 1)
+        self.assertRedirects(response, f'/products/{product.id}')
+
+    def test_add_to_bag_with_same_spice_index_product(self):
+        product = Product.objects.create(
+                name="Item", price=50, category_id=1, spice_index=True)
+        post_data = {
+            'quantity': '1',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data)
+        post_data_two = {
+            'quantity': '1',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data_two)
+        bag = self.client.session.get('bag', {})
+        print(bag)
+        self.assertEqual(len(bag), 1)
+        self.assertRedirects(response, f'/products/{product.id}')
+
+    def test_add_to_bag_product_exists(self):
+        product = Product.objects.create(
+                name="Item", price=50, category_id=1, spice_index=True)
+        # Add 1st product
+        post_data = {
+            'quantity': '1',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data)
+        # Add same product again
+        post_data_two = {
+            'quantity': '1',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data_two)
+        bag = self.client.session.get('bag', {})
+        self.assertEqual(
+            bag['1'], 2)
+        self.assertEqual(len(bag), 1)
+        self.assertRedirects(response, f'/products/{product.id}')
+
     """ Test Adjusting items in bag """
-    def test_adjust_bag(self):
+    def test_adjust_bag_no_spice_index(self):
         product = Product.objects.create(
                 name="Item", price=50, category_id=1, spice_index=True)
         # Add tester item to bag
@@ -51,6 +111,26 @@ class BagTestViews(TestCase):
             'adjust_bag', args=[product.id]), data=post_data_two)
         bag2 = self.client.session.get('bag')
         self.assertEqual(bag2['1'], int(post_data_two['quantity']))
+        self.assertRedirects(response, '/bag/')
+
+    """ Test Adjusting items in bag """
+    def test_adjust_bag_with_spice_index(self):
+        product = Product.objects.create(
+                name="Item", price=50, category_id=1, spice_index=True)
+        # Add tester item to bag
+        post_data = {
+            'quantity': '1',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data)
+        # https://docs.djangoproject.com/en/3.1/topics/testing/tools/#django.test.Client.session
+        post_data_two = {
+            'quantity': '2',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'adjust_bag', args=[product.id]), data=post_data_two)
         self.assertRedirects(response, '/bag/')
 
     """ Test deleting items from bag """
