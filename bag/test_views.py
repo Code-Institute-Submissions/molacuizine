6acbbd3558c8c0b8ae5746c27a0ec7ceaa118ab3
zahur76 <zahurmeerun@hotmail.com>
2021-01-21 08:24:13 +0000

    Add test case to test adding items to bag

--- bag/test_views.py
+++ bag/test_views.py
@@ -1,11 +1,31 @@
 from django.test import TestCase
+from bag.models import Store
+from products.models import Product, Category
+from django.shortcuts import reverse
 
 
 # Create your tests here.
-class TestBagViews(TestCase):
+class BagTestViews(TestCase):
+
+    def setUp(self):
+        self.Store = Store.objects.create(store_status='open')
+        self.Category = Category.objects.create(name='sides')
 
     def test_view_bag(self):
         response = self.client.get('/bag/')
-        print(response)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
+
+    """ Test adding items to bag """
+    def test_add_to_bag(self):
+        product = Product.objects.create(
+                name="Item", price=50, category_id=1, spice_index=True)
+        post_data = {
+            'quantity': '1',
+            'spice_index': 'mild',
+        }
+        response = self.client.post(reverse(
+            'add_to_bag', args=[product.id]), data=post_data)
+        self.assertRedirects(response, f'/products/{product.id}')
+
+   
