9fdf66419425ec7e48a3147e29fa184cb71a5557
zahur76 <zahurmeerun@hotmail.com>
2021-01-21 05:00:32 +0000

    Remove print statement from test file in home app

--- home/test_views.py
+++ home/test_views.py
@@ -2,12 +2,6 @@ from django.test import TestCase
 from django.test import Client
 from bag.models import Store
 
-c = Client()
-url = '/bag/'
-r = c.get(url)
-print(r)
-print(r.status_code)
-print(r.status_code)
 
 # Create your tests here.
 class TestViews(TestCase):
@@ -17,6 +11,5 @@ class TestViews(TestCase):
 
     def test_view_bag(self):
         response = self.client.get('/')
-        print(response)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
