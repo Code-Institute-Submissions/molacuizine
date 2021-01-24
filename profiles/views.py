5ebef41c6f2b2e1ebca8aaec5376a647ec0f52bc
zahur76 <zahurmeerun@hotmail.com>
2021-01-24 05:26:29 +0000

    Add ridirect templates to profile view when submitting form

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from .models import UserProfile
 from checkout.models import Order
@@ -12,31 +12,28 @@ def profile(request):
     """ Display the user's profile. """
 
     # Request profile for logged in user
-    profile = get_object_or_404(UserProfile, user__username=request.user)
+    profile = get_object_or_404(UserProfile, user__username=request.user)   
     # Obtain all orders for userprofile called above
     orders = profile.orders.all()
 
     if request.method == 'POST':
-
         form = UserProfileForm(request.POST, instance=profile)
-
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+            return redirect(reverse('profile'))
         else:
             messages.error(
                 request, 'Update failed. Please ensure the form is valid.')
-
+            return redirect(reverse('profile'))
     if request.method == 'GET':
-
         form = UserProfileForm(instance=profile)
-
-    context = {
-        'form': form,
-        'profile': profile,
-        'orders': orders,
-    }
-    return render(request, 'profiles/profile.html', context)
+        context = {
+            'form': form,
+            'profile': profile,
+            'orders': orders,
+        }
+        return render(request, 'profiles/profile.html', context)
 
 
 @login_required
