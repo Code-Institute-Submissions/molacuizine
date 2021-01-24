f6ad43f8be43d8de0023a45d3369f75306941ee5
zahur76 <zahurmeerun@hotmail.com>
2021-01-24 14:37:54 +0000

    Add availabillity to product model

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     spice_index = models.BooleanField(default=False, null=True, blank=True)
+    availability = models.BooleanField(default=True, null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
