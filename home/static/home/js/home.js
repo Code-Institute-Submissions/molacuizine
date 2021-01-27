391120fe50b3526eafdda2ac509237b089e2b839
zahur76 <zahurmeerun@hotmail.com>
2021-01-27 14:29:24 +0000

    Add missing semi colon for jshint validator compliance

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
     $(".open").click(function(){
         $("#deliveryModal").modal('show');
-    })
+    });
 
     $(".close").click(function(){
         $("#deliveryModal").modal('hide');
-    })
+    });
 });
