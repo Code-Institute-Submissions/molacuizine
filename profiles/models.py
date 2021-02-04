14bf397a2203b04ea796eb8613655fc6c6ac0519
zahur76 <zahurmeerun@hotmail.com>
2021-02-04 12:47:43 +0000

    Modify profile model to include validation for phone number

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import RegexValidator
 
 
 class Town(models.Model):
@@ -29,7 +30,8 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(
-        max_length=20, null=True, blank=True)
+        max_length=8, null=True, blank=True, validators=[RegexValidator(
+            r'^\d{8}$', message='Incorrect number format')])
     default_street_address1 = models.CharField(
         max_length=80, null=True, blank=True)
     town = models.ForeignKey(
